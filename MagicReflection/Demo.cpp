7240cbb615638667f22f663ddd3c5b6cf296538c
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2024-01-29 18:27:28 +0100

    create MRFL_GET_TYPE_NAME macro

--- MagicReflection/Demo.cpp
+++ MagicReflection/Demo.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+#include "MagicReflection.h"
+
+struct FooBar
+{
+
+};
+
+template<typename T, typename U>
+class TemplatedClass
+{
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    {
+        FooBar Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        std::vector<FooBar> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        std::map<int, int> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        std::unordered_map<int, FooBar> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        TemplatedClass<int, int> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        TemplatedClass<double, float> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
+
+    {
+        TemplatedClass<int, FooBar> Foo{};
+        std::cout << MRFL_GET_TYPE_NAME(Foo) << "\n\n";
+    }
 }
--- MagicReflection/MagicReflection.h
+++ MagicReflection/MagicReflection.h
@@ -1,6 +1,51 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+
+#define MRFL_GET_TYPE_NAME(object) MRFL::GetTypename<decltype(object)>()
+
+#ifdef _MSC_VER
+#define MRFL_FUNCTION_NAME __FUNCSIG__
+#else
+#define MRFL_FUNCTION_NAME __PRETTY_FUNCTION__
+#endif
+
+#define MRFL_CONSTEXPR constexpr
+#define MRFL_NODISCARD [[nodiscard]]
+#define MRFL_MAX(a, b) a < b ? b : a
+
 namespace MRFL // MagicReFLection
 {
+	namespace Implementation
+	{
+		template<typename T>
+		MRFL_NODISCARD MRFL_CONSTEXPR static std::string_view GetFullTypename()
+		{
+			return MRFL_FUNCTION_NAME;
+		}
 
-}
+		template<typename T>
+		MRFL_NODISCARD MRFL_CONSTEXPR static std::string_view GetFilteredTypename()
+		{
+			// This is pretty stupid.
+			MRFL_CONSTEXPR static std::string_view FunctionName{ "MRFL::Implementation::GetFullTypename" };
+
+			MRFL_CONSTEXPR static std::string_view FullName{ GetFullTypename<T>() };
+			MRFL_CONSTEXPR static std::string_view FilteredName{ FullName.substr(FullName.find(FunctionName) + FunctionName.size()) };
+
+			// FilteredName will look like:
+			// <int, int>
+			// <struct FooBar, int>
+			// <int, class Map<struct FooBar, int>>
+
+			return FilteredName.substr(1, FilteredName.find_last_of('>') - 1);
+		}
+	} // Implementation
+
+	template<typename T>
+	MRFL_NODISCARD MRFL_CONSTEXPR static std::string GetTypename()
+	{
+		return std::string{ Implementation::GetFilteredTypename<T>() };
+	}
+} // MRFL
